9bb122a01e597241e1b7ebc0e6392ad5c144c2b6
Sagorhowlader <sagor.iubat17@gmail.com>
2023-01-10 12:44:36 +0600

    done

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# ecommerce-site-scrape
+# LuluHyperMarket Website Product  Details Webscraping
+
+The code provided is a web scraper that extracts data about a product from a website. It makes an HTTP request to the specified URL and parses the HTML content of the response using the `scrapy` library.
+
+### Requirements
+
+- Python 3.8.0+
+- Scrapy 2.7.1+
+- requests
+
+
+
+### Installation
+
+A step by step guide on how to get the development environment running.
+
+**1. Clone the repository**
+
+     git clone https://github.com/Sagorhowlader/ecommerce-site-scrape
+
+**2. Navigate to the project directory**
+
+    cd ecommerce_site_scrape
+
+**3. Install the dependencies**
+
+    pip install -r requirements.txt
+
+
+**5. Running the project**
+
+    scrapy runspider main.py
+
+**6. For save json in file run**
+
+    scrapy runspider main.py -o data.json
+
+`data.json` file save the data as 
+
+```json
+{
+  "product_title": "PRODUCT TITLE",
+  "product_price": "PRODUCT PRICE",
+  "product_summary": "PRODUCT SUMMARY"[]
+}
+```
+
+    
--- main.py
+++ main.py
@@ -1,11 +1,43 @@
+import re
+
 import scrapy
-from scrapy.linkextractors import LinkExtractor
+
+
+def clean_string(string):
+    """This function takes in a string and returns a cleaned version of that string. It removes leading and trailing
+    whitespace, replaces non-breaking spaces with regular spaces, replaces hyphens with spaces, removes commas,
+    and removes any remaining unnecessary characters."""
+
+    # Strip leading and trailing whitespace
+    string = string.strip()
+
+    # Replace non-breaking spaces with regular spaces
+    string = string.replace("\u00a0", " ")
+
+    # Replace hyphens with spaces
+    string = string.replace("–", " ")
+
+    # Remove commas
+    string = string.replace(",", "")
+
+    # Remove any remaining unnecessary characters
+    string = re.sub(r'[^\w\s\.\']', '', string)
+
+    return string
+
 
 class LuluHypermarketSpider(scrapy.Spider):
+    # The name attribute in the LuluHypermarketSpider class is used to give the spider a unique identifier.
     name = 'lulu_hypermarket'
+
+    # start_urls is a list of URLs that the spider will begin to crawl from
     start_urls = ['https://www.luluhypermarket.com/en-ae/electronics']
 
     def parse(self, response):
+        """This is the main parsing function for the LuluHypermarketSpider spider. It extracts the URLs for the
+        subcategories and products from the provided response and yields a request for each subcategory URL,
+        calling the parse_subcategory function for each subcategory"""
+
         # Find the container element that holds the subcategories and products
         container = response.css('div.section-container.recommended-content')
         # Extract the URLs for the subcategories and products
@@ -15,6 +47,8 @@ class LuluHypermarketSpider(scrapy.Spider):
             yield scrapy.Request(subcategory_url, callback=self.parse_subcategory)
 
     def parse_subcategory(self, response):
+        """This function extracts the URLs for the products from the provided response and yields a request for each
+        product URL, calling the parse_product function for each product."""
         container = response.css('div.product-box')
         # Extract the URLs for the products
         products_urls = container.css('a::attr(href)').extract()
@@ -23,23 +57,33 @@ class LuluHypermarketSpider(scrapy.Spider):
             yield scrapy.Request(product_url, callback=self.parse_product)
 
     def parse_product(self, response):
-        # Extract the price
-        price = response.css('div.price-tag.detail span.current span.item.price span::text').extract_first()
-        # Extract the title
-        title = response.css('h1.product-name::text').extract_first()
-        # Extract the product summary
-        summary = {}
-        summary_description = response.css('div.description-block')
-        summary_items = summary_description.css('li::text').getall()
+        """This function extracts the product title, price, and summary from the provided response and stores them in
+        a dictionary. It then yields the dictionary as the final output."""
 
-        for summary_item in summary_items:
-            print(summary_item)
-        #     key = summary_item.css('div.key::text').extract_first()
-        #     value = summary_item.css('div.value::text').extract_first()
-        #     summary[key] = value
-        #
-        # yield {
-        #     'price': price,
-        #     'title': title,
-        #     'summary': summary
-        # }
+        # Extract the product details container
+        product_description = response.css('.product-description')
+
+        # Extract the product title
+        product_title = product_description.css('h1.product-name::text').extract_first()
+
+        # Extract the product price
+        price = product_description.css('div.price-tag span.current span.item.price span::text').extract_first()
+
+        # Extract the product summary container
+        description_block = product_description.css('.description-block')
+
+        # Extract summary details in list
+        description_items = description_block.css('li::text').extract()
+
+        # Clean summary details string
+        product_summary = [clean_string(item) for item in description_items if item is not None]
+
+        # Create a dictionary to hold the extracted data
+        product_data = {
+            'product_title': clean_string(product_title) if product_title is not None else None,
+            'product_price': price,
+            'product_summary': product_summary
+        }
+
+        # Yield the data as a dictionary
+        yield product_data
